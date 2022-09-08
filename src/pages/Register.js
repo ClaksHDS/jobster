cc0878051486c8772fc39c9ee74c27d2a4806632
Your Namehuetclariss <you@example.com>
2022-09-08 11:10:31 +0100

    React Redux Toolkit
    Install the package react redux toolkit + axios library, create userSlice, store file that we pass on to the wrap our App in index.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { Logo, FormRow } from "../components";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
 //import { useNavigate } from "react-router-dom";
 
 const initialState = {
@@ -13,6 +15,8 @@ const initialState = {
 };
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -25,7 +29,13 @@ const Register = () => {
     const { name, email, password, isMember } = values;
     if (!email || !password || (!isMember && !name)) {
       toast.error("Please fill out all fields");
+      return;
     }
+    if (isMember) {
+      dispatch(loginUser({ email, password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
 
   const toggleMember = () => {
