a20148c3c86050651adeb66ced29d5649734a26e
Your Namehuetclariss <you@example.com>
2022-09-12 10:17:09 +0100

    AllJobs - work on JobsContainer
    create loading and jobInfo components . Install Moment library to set up the date in the info. Set up the toggleLoading by adding reducers to allJobsSlice and import it in JobSlice

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/Job";
+import { useDispatch } from "react-redux";
+import JobInfo from "./jobInfo";
+import moment from "moment/moment";
 
-const Job = () => {
-  return <h2>Job</h2>;
+const Job = ({
+  _id,
+  company,
+  createdAt,
+  jobLocation,
+  jobType,
+  position,
+  status,
+}) => {
+  const dispatch = useDispatch();
+
+  const date = moment(createdAt).format("MMM Do YYYY");
+
+  return (
+    <Wrapper>
+      <header>
+        <div className='main-icon'>{company.slice(0, 1)}</div>
+        <div className='info'>
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className='content'>
+        <div className='content-center'>
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className='actions'>
+            <Link
+              to='/add-job'
+              className='btn edit-btn'
+              onClick={() => console.log("edit job")}
+            >
+              edit{""}
+            </Link>
+            <button
+              type='button'
+              className='btn delete-btn'
+              onClick={() => console.log("delete job")}
+            >
+              delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Job;
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -11,7 +11,7 @@ const initialFiltersState = {
 };
 
 const initialState = {
-  isLoading: false,
+  isLoading: true,
   jobs: [],
   totalJobs: 0,
   numOfPage: 1,
@@ -20,9 +20,47 @@ const initialState = {
   ...initialFiltersState,
 };
 
+export const getAllJobs = createAsyncThunk(
+  "allJobs/ getJobs",
+  async (_, thunkAPI) => {
+    let url = `/jobs`;
+    try {
+      const response = await customFetch.get(url, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token} `,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
+  reducers: {
+    showLoading: (state) => {
+      state.isLoading = true;
+    },
+    hideLoading: (state) => {
+      state.isLoading = false;
+    },
+  },
+  extraReducers: {
+    [getAllJobs.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getAllJobs.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.jobs = payload.jobs;
+    },
+    [getAllJobs.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
 });
 
+export const { showLoading, hideLoading } = allJobsSlice.actions;
 export default allJobsSlice.reducer;
