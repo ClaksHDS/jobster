b5a332623e6ebf4e5aa27c14be09d93a968da6a6
Your Namehuetclariss <you@example.com>
2022-09-12 12:30:49 +0100

    Add Job Page
    Delete job request, set edit job, edit job request

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -7,6 +7,7 @@ import {
   handleChange,
   clearValues,
   createJob,
+  editJob,
 } from "../../features/job/jobSlice";
 import { useEffect } from "react";
 
@@ -34,6 +35,15 @@ const AddJob = () => {
       toast.error("Please fill out all fields");
       return;
     }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status },
+        })
+      );
+      return;
+    }
     dispatch(
       createJob({
         position,
@@ -52,7 +62,9 @@ const AddJob = () => {
   };
 
   useEffect(() => {
-    dispatch(handleChange({ name: "jobLocation", value: user.location }));
+    if (!isEditing) {
+      dispatch(handleChange({ name: "jobLocation", value: user.location }));
+    }
   }, []);
 
   return (
